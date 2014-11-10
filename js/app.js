bb73229208f9b06a6e16edcd299dcde9b895a56c
Yu KATO <Yu@Yu-no-MacBook-Pro.local>
2014-11-10 12:47:05 +0900

    create sphere

--- js/app.js
+++ js/app.js
@@ -11,6 +11,9 @@ var app = (function(){
         vs, // vertex shader
         fs; // fragment shader
 
+    // private context
+    var _this = this;
+
     // #pragma region - private methods
     var _setVertexShader = function (shader) {
       vs = shader;
@@ -38,7 +41,10 @@ var app = (function(){
         color.push (r[i], g[i], b[i], a);
       }
       return color;
-    }
+    };
+
+    // #pragma region - public variables
+    this.texture = null;
 
     // #pragma region - public methods
     this.init = function(w, h, clearColor, depth){
@@ -50,13 +56,13 @@ var app = (function(){
       gl.clearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
       gl.clearDepth (depth);
       gl.clear (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    }
+    };
 
     this.clearContext = function (clearColor, depth) {
       gl.clearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
       gl.clearDepth (depth);
       gl.clear (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    }
+    };
 
     // getter, setter
     this.getVertexShader = function () {
@@ -70,11 +76,11 @@ var app = (function(){
 
     this.getCanvas = function () {
       return c;
-    }
+    };
 
     this.getContext = function () {
       return gl;
-    }
+    };
 
     this.createShader = function (id) {
       var shader = null;
@@ -146,7 +152,7 @@ var app = (function(){
         // if failure, throw error
         throw new Error (gl.getProgramInfoLog (program));
       }
-    }
+    };
 
     this.createVBO = function (data) {
       var vbo = gl.createBuffer ();
@@ -161,7 +167,7 @@ var app = (function(){
       gl.bindBuffer (gl.ARRAY_BUFFER, null);
 
       return vbo;
-    }
+    };
 
     this.createIBO = function (data) {
       var ibo = gl.createBuffer ();
@@ -171,6 +177,21 @@ var app = (function(){
       gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, null);
 
       return ibo;
+    };
+
+    this.createTexture = function (source) {
+      var img = new Image ();
+
+      img.onload = function () {
+        var tex = gl.createTexture;
+        gl.bindTexture (gl.TEXTURE_2D, tex);
+        gl.texImage2D (gl.TEXTURE_2D, 0, gl.RGBA, gl.UNSIGNED_BYTE, img);
+        gl.generateMipmap (gl.TEXTURE_2D);
+        gl.bindTexture (gl.TEXTURE_2D, null);
+
+        _this.texture = tex;
+      }
+      img.src = source;
     }
 
     this.setShaderAttrib = function (vbo, attL, attS) {
@@ -181,9 +202,9 @@ var app = (function(){
         gl.vertexAttribPointer (attL[i], attS[i], gl.FLOAT, false, 0, 0);
       }
 
-    }
+    };
 
-    this.createTorus = function (row, column, irad, orad) {
+    this.createTorus = function (row, column, irad, orad, color) {
       var pos = [], col = [], idx = [], nor = [];
       for (var i=0; i <= row; i++) {
         var r = Math.PI * 2 / row * i;
@@ -197,9 +218,13 @@ var app = (function(){
           var tz = (rr * irad + orad) * Math.sin (tr);
           var rx = rr * Math.cos (tr);
           var rz = rr * Math.sin (tr);
+          if (color) {
+            var tc = color;
+          } else {
+            var tc = _hsva (360 / column * ii, 1, 1, 1);
+          }
           pos.push (tx, ty, tz);
           nor.push (rx, ry, rz);
-          var tc = _hsva (360 / column * ii, 1, 1, 1);
           col.push (tc[0], tc[1], tc[2], tc[3]);
         }
       }
@@ -210,9 +235,45 @@ var app = (function(){
           idx.push (r + column + 1, r + column + 2, r + 1);
         }
       }
-      return [pos, nor, col, idx];
+      return {p: pos, n:nor, c:col, i:idx};
+    };
+
+    this.createSphere = function (row, column, rad, color) {
+        var pos = new Array(), nor = new Array(),
+            col = new Array(), idx = new Array();
+        for(var i = 0; i <= row; i++){
+            var r = Math.PI / row * i;
+            var ry = Math.cos(r);
+            var rr = Math.sin(r);
+            for(var ii = 0; ii <= column; ii++){
+                var tr = Math.PI * 2 / column * ii;
+                var tx = rr * rad * Math.cos(tr);
+                var ty = ry * rad;
+                var tz = rr * rad * Math.sin(tr);
+                var rx = rr * Math.cos(tr);
+                var rz = rr * Math.sin(tr);
+                if (color) {
+                    var tc = color;
+                } else {
+                    var tc = _hsva(360 / row * i, 1, 1, 1);
+                }
+                pos.push(tx, ty, tz);
+                nor.push(rx, ry, rz);
+                col.push(tc[0], tc[1], tc[2], tc[3]);
+            }
+        }
+        r = 0;
+        for(i = 0; i < row; i++){
+            for(ii = 0; ii < column; ii++){
+                r = (column + 1) * i + ii;
+                idx.push(r, r + 1, r + column + 2);
+                idx.push(r, r + column + 2, r + column + 1);
+            }
+        }
+        return {p : pos, n : nor, c : col, i : idx};
     }
-  }
+
+  };
 
 
   // return singleton class.
@@ -230,44 +291,51 @@ window.onload = function(){
   // set clear color RGBA
   var clearColorArray = [0.0, 0.0, 0.0, 1.0];
 
-  app.init(300, 300, clearColorArray, 1.0);
+  app.init(500, 300, clearColorArray, 1.0);
   var c = app.getCanvas ();
   var gl = app.getContext ();
-  var vShader = app.createShader ('vertexShader');
-  var fShader = app.createShader ('fragmentShader');
+  var vShader = app.createShader ('vs');
+  var fShader = app.createShader ('fs');
   var prg = app.createProgram (vShader, fShader);
 
-
-  var torusData = app.createTorus (32, 32, 1.0, 2.0);
-  var position = torusData[0];
-  var normal = torusData[1]
-  var color = torusData[2];
-  var index = torusData[3];
-
-  // create VBO
-  var vbos = new Array (3);
-  vbos[0] = app.createVBO (position);
-  vbos[1] = app.createVBO (color);
-  vbos[2] = app.createVBO (normal);
-
-  var attLocation = new Array (3);
+  var attLocation = [];
   attLocation[0] = gl.getAttribLocation (prg, 'position');
   attLocation[1] = gl.getAttribLocation (prg, 'color');
   attLocation[2] = gl.getAttribLocation (prg, 'normal');
 
-  var attStride = new Array (2);
+  var attStride = [];
   attStride[0] = 3;
   attStride[1] = 4;
   attStride[2] = 3;
-  app.setShaderAttrib (vbos, attLocation, attStride);
 
-  // create IBO
-  var ibo = app.createIBO (index);
-  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, ibo);
 
-  gl.enable (gl.CULL_FACE);
-  gl.enable (gl.DEPTH_TEST);
-  gl.depthFunc (gl.LEQUAL);
+  // create torus
+  var torusData = app.createTorus (64, 64, 0.5, 1.5, [0.75, 0.25, 0.25, 1.0]);
+  var tPosition = app.createVBO (torusData.p);
+  var tNormal = app.createVBO (torusData.n);
+  var tColor = app.createVBO (torusData.c);
+  var tVBOList = [tPosition, tColor, tNormal];
+  var tIndex = torusData.i;
+  var tIBO = app.createIBO (tIndex);
+
+  // create sphere
+  var sphereData = app.createSphere (64, 64, 2.0, [0.25, 0.25, 0.75, 1.0]);
+  var sPosition = app.createVBO (sphereData.p);
+  var sNormal = app.createVBO (sphereData.n);
+  var sColor = app.createVBO (sphereData.c);
+  var sVBOList = [sPosition, sColor, sNormal];
+  var sIndex = sphereData.i;
+  var sIBO = app.createIBO (sIndex);
+
+
+  var uniLocation = [];
+  uniLocation[0] = gl.getUniformLocation (prg, 'mvpMatrix');
+  uniLocation[1] = gl.getUniformLocation (prg, 'mMatrix');
+  uniLocation[2] = gl.getUniformLocation (prg, 'invMatrix');
+  uniLocation[3] = gl.getUniformLocation (prg, 'lightPosition');
+  uniLocation[4] = gl.getUniformLocation (prg, 'eyeDirection');
+  uniLocation[5] = gl.getUniformLocation (prg, 'ambientColor');
+
   // create mat4 lib instance
   var m = new matIV ();
 
@@ -279,23 +347,19 @@ window.onload = function(){
   var mvpMatrix = m.identity (m.create ());
   var invMatrix = m.identity(m.create());
 
-  m.lookAt ([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
-  m.perspective (45, c.width / c.height, 0.1, 100, pMatrix);
-  m.multiply (pMatrix, vMatrix, tmpMatrix);
-  m.inverse (mMatrix, invMatrix);
-
-  var lightDirection = [-0.5, 0.5, 0.5];
+  var lightPosition = [0.0, 0.0, 0.0];
   var ambientColor = [0.1, 0.1, 0.1, 1.0];
   var eyeDirection = [0.0, 0.0, 20.0];
 
-  var uniLocation = [];
-  uniLocation[0] = gl.getUniformLocation (prg, 'mvpMatrix');
-  uniLocation[1] = gl.getUniformLocation (prg, 'invMatrix');
-  uniLocation[2] = gl.getUniformLocation (prg, 'lightDirection');
-  uniLocation[3] = gl.getUniformLocation (prg, 'ambientColor');
-  uniLocation[4] = gl.getUniformLocation (prg, 'eyeDirection');
+  m.lookAt (eyeDirection, [0, 0, 0], [0, 1, 0], vMatrix);
+  m.perspective (45, c.width / c.height, 0.1, 100, pMatrix);
+  m.multiply (pMatrix, vMatrix, tmpMatrix);
 
   var count = 0;
+
+  gl.enable (gl.CULL_FACE);
+  gl.enable (gl.DEPTH_TEST);
+  gl.depthFunc (gl.LEQUAL);
 
   var animation = (function (w, r) {
     return w['r'+r] = w['r'+r] || w['webkitR'+r] || w['mozR'+r] || w['msR'+r] || w['oR'+r] || function(c){ w.setTimeout(c, 1000 / 60); };
@@ -306,19 +370,42 @@ window.onload = function(){
     app.clearContext(clearColorArray, 1.0);
     count++;
     var rad = (count % 360) * Math.PI / 180;
+    var tx = Math.cos (rad) * 3.5;
+    var ty = Math.sin (rad) * 3.5;
+    var tz = Math.sin (rad) * 3.5;
+
+    // render torus
+    app.setShaderAttrib (tVBOList, attLocation, attStride);
+    gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, tIBO);
 
     m.identity (mMatrix);
-    m.rotate (mMatrix, rad, [1, 0, 1], mMatrix);
+    m.translate (mMatrix, [tx, -ty, -tz], mMatrix);
+    m.rotate (mMatrix, -rad, [0, 1, 1], mMatrix);
     m.multiply (tmpMatrix, mMatrix, mvpMatrix);
     m.inverse (mMatrix, invMatrix);
 
     gl.uniformMatrix4fv (uniLocation[0], false, mvpMatrix);
-    gl.uniformMatrix4fv (uniLocation[1], false, invMatrix);
-    gl.uniform3fv (uniLocation[2], lightDirection);
-    gl.uniform4fv (uniLocation[3], ambientColor);
+    gl.uniformMatrix4fv (uniLocation[1], false, mMatrix);
+    gl.uniformMatrix4fv (uniLocation[2], false, invMatrix);
+    gl.uniform3fv (uniLocation[3], lightPosition);
     gl.uniform3fv (uniLocation[4], eyeDirection);
+    gl.uniform4fv (uniLocation[5], ambientColor);
+    gl.drawElements(gl.TRIANGLES, torusData.i.length, gl.UNSIGNED_SHORT, 0);
 
-    gl.drawElements(gl.TRIANGLES, index.length, gl.UNSIGNED_SHORT, 0);
+
+    // render sphere
+    app.setShaderAttrib (sVBOList, attLocation, attStride);
+    gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, sIBO);
+
+    m.identity (mMatrix);
+    m.translate(mMatrix, [-tx, ty, tz], mMatrix);
+    m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+    m.inverse(mMatrix, invMatrix);
+
+    gl.uniformMatrix4fv (uniLocation[0], false, mvpMatrix);
+    gl.uniformMatrix4fv (uniLocation[1], false, mMatrix);
+    gl.uniformMatrix4fv (uniLocation[2], false, invMatrix);
+    gl.drawElements (gl.TRIANGLES, sphereData.i.length, gl.UNSIGNED_SHORT, 0);
 
     gl.flush ();
     animation(render);
